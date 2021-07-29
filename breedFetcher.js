d7a91bd61af0472c19c1fb632beeac41d9f7b0ca
Anthony Kim <fbssaejun@gmail.com>
2021-07-29 22:19:02 +0000

    edited considering different test cases

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,24 +1,50 @@
-const fetch = require("node-fetch");
-let arg = process.argv[2];
+const request = require("request");
 
-const getBreed = (breed) => {
-  fetch(`https://api.hecatapi.com/v1/breeds/search?q=${breed}`)
-    .then(function(response) {
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (err,response, body) => {
+    const data = JSON.parse(body);
+    if (response === null) {
+      callback(err, null)
+      return
+    }
 
-      return response.json(); //JSON.parse(response)
-    })
-    .then(function(myJson) {
-      const breedResult = myJson[0];
-      if (!breedResult) {
-        console.log("No Breed Found!");
-        return;
-      }
-      console.log(breedResult.description);
-    })
-    .catch((e) => {
-      console.log("No data found!");
-      console.log(e);
-    });
+    if (data[0] === undefined) {
+      callback('No matching breeds', null);
+      return;
+    }
+
+    callback(null, data[0]['description']);
+
+  });
 };
 
-getBreed(arg);
+module.exports = {fetchBreedDescription};
+
+
+//Another alternative way using fetch() method
+
+// const fetch = require("node-fetch");
+
+
+// const fetchBreedDescription = (breedName) => {
+//   fetch(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`)
+//     .then(function(response) {
+//       return response.json();
+//     })
+//     .then(function(myJson) {
+//       const breedResult = myJson[0];
+//       console.log(breedResult)
+//       if (!breedResult) {
+//         console.log("No Breed Found!");
+//         return;
+//       }
+//       console.log(breedResult.description);
+//     })
+//     .catch((e) => {
+//       console.log("No data found!");
+//       console.log(e);
+//     });
+// };
+
+
+// call function with one parameter (breedName) in index js
